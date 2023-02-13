0d60d5af488c5f5644d8d3066f63989cfe856716
Frank Schumann <frank.schumann.76@gmail.com>
2023-02-13 23:43:37 +0100

    add conan test_package

---
+++ test_package/src/example.cpp
@@ -0,0 +1,15 @@
+#include "core/component/ComponentController.h"
+#include "core/component/ComponentControllerIf.h"
+#include "core/component/ComponentIf.h"
+
+#include "core/factory/FactoryController.h"
+#include "core/factory/FactoryControllerIf.h"
+#include "core/factory/FactoryIf.h"
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "core" << std::endl;
+}
+
